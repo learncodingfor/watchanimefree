87532eb2808daf867fb5a9b38a6b4603e6f46837
apple <78810546+pytube832@users.noreply.github.com>
2021-08-25 16:01:16 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-
+var script = document.createElement("script");
+script.src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+document.body.appendChild(script);
+$(document).resdy(function(){
+  $("div").animate({left: '250px'})
+});
