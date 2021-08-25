5dc30a2ab79699a08102244ddbbb506f05404b02
apple <78810546+pytube832@users.noreply.github.com>
2021-08-25 16:35:53 -0400

    Update module.js

--- js/module.js
+++ js/module.js
@@ -1,6 +1,7 @@
 export function anime(){
   return "anime";
   alert("thank you for visiting us");
+  window.location.assign("genre.html");
 }
 export function welcome(){
   alert("Welcome to my website");
