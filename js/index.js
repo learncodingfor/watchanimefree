fd563f7ef517b34e1eb6b9eeaa54cf27cde1901d
apple <78810546+pytube832@users.noreply.github.com>
2021-08-25 16:13:51 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-var script = document.createElement("script");
-script.src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
-document.body.appendChild(script);
-$(document).ready(function(){
-  $("div").animate({height: '300px',width: '200px'})
-  $("div").text("watchanimeforfree")
-});
+document.body.style.display="none";
+document.setTimeout(function(){
+document.body.style.display ="block";},3000);
